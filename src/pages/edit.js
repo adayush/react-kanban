c43a6ba778ae4d3ee9ab892276c2c75d9817d92a
adayush <adayush0@gmail.com>
2022-10-16 21:56:03 +0530

    Change status functionality

--- src/pages/edit.js
+++ src/pages/edit.js
@@ -10,33 +10,66 @@ const Page = ({ data, setData, deleteTask }) => {
 
   const [values, setValues] = useState({
     title: data[statusIndex].tasks[taskIndex].title,
-    status: data[statusIndex].title,
-    description: data[statusIndex].tasks[taskIndex].description
+    status: data[statusIndex].id,
+    description: data[statusIndex].tasks[taskIndex].description,
+    source: undefined,
+    target: undefined
   })
-  //console.log(values)
+
   useEffect(() => {
-    //console.log('changed')
+    // console.log(values)
+    let flag = false
+    
     const newData = [...data]
-    newData[statusIndex].tasks[taskIndex].title = values.title
-    newData[statusIndex].tasks[taskIndex].description = values.description
+    const tmpTask = newData[statusIndex].tasks[taskIndex]
+    tmpTask.title = values.title
+    tmpTask.description = values.description
+    newData[statusIndex].tasks[taskIndex] = tmpTask
+
+    if (values.target && (statusIndex !== values.target)) {
+      newData[statusIndex].tasks.splice(taskIndex, 1)
+      newData[values.target].tasks.push(tmpTask)
+      flag = true
+    }
+
     setData(newData)
     localStorage.setItem('data', JSON.stringify(data))
-    // newData[status]
-  }, [values, statusIndex, taskIndex])
 
-  const handleChange = (event, type) => {
-    if (type === 'title') {
+    if (flag)
+      navigate(`/edit/${values.target}/${newData[values.target].tasks.length-1}`)
+    // newData[status]
+  }, [values, statusIndex, taskIndex]) // eslint-disable-line react-hooks/exhaustive-deps
+
+  const handleChange = ({target}, type) => {
+    if (type !== 'status') {
       setValues({
         ...values,
-        [type]: event.target.value
+        [type]: target.value,
+        target: undefined
       })
-    } else if (type === 'description') {
-      //console.log(event)
+    } else {
       setValues({
         ...values,
-        [type]: event.target.value
+        [type]: target.value,
+        target: target.options[target.options.selectedIndex].getAttribute('target')
       })
     }
+    // if (type === 'title') {
+    //   setValues({
+    //     ...values,
+    //     [type]: event.target.value
+    //   })
+    // } else if (type === 'description') {
+    //   setValues({
+    //     ...values,
+    //     [type]: event.target.value
+    //   })
+    // } else if (type === "status") {
+    //   setValues({
+    //     ...values,
+    //     [type]: event.target.value
+    //   })
+    // }
   }
 
   return (
@@ -67,7 +100,11 @@ const Page = ({ data, setData, deleteTask }) => {
         </div>
         <br />
         <div className={style.status}>
-          <p>{values.status}</p>
+          <select value={values.status} onChange={(e) => handleChange(e, 'status')}>
+            {data.map((status, i) => (
+              <option key={i} value={status.id} target={i}>{status.title}</option>
+            ))}
+          </select>
         </div>
         <div className={style.divider}></div>
         <div className={style.description}>
